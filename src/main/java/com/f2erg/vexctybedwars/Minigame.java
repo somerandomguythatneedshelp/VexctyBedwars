7c075ddba489847058a32a8250ce90b22d36292f
Taha Akhtar <sunseedyay@gmail.com>
2022-12-08 16:49:26 +0000

    added files

---
+++ src/main/java/com/f2erg/vexctybedwars/Minigame.java
@@ -0,0 +1,17 @@
+package com.f2erg.vexctybedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Minigame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
